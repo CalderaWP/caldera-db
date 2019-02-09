4663cbe94f0259879e7fd6a3e71a3ee70d60ff5c
Josh Pollock <josh@calderawp.com>
2019-02-09 15:29:15 -0500

    handle updated at/ created at for message

---
+++ tests/Unit/TimeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace calderawp\DB\Tests\Unit;
+
+use calderawp\DB\Time;
+
+class TimeTest extends TestCase
+{
+
+	public function testNormalizeMysql()
+	{
+		$timestamp = '2017-06-30 11:37:06';
+		$result = Time::normalizeMysql($timestamp);
+		$this->assertEquals('2017-06-30 11:37:06',$result);
+	}
+
+	public function testDateTimeFromMysql()
+	{
+		$timestamp = '2017-06-30 11:37:06';
+		$result = Time::dateTimeFromMysql($timestamp);
+		$this->assertEquals($timestamp, $result->format(Time::FORMAT));
+	}
+}
