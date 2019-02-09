80d7a80e5eeac8fc1a20569d5e2174c862d55b45
Josh Pollock <josh@calderawp.com>
2019-02-09 18:45:34 -0500

    run fixes

--- src/Time.php
+++ src/Time.php
@@ -7,27 +7,27 @@ namespace calderawp\DB;
 class Time
 {
 
-	const FORMAT = 'Y-m-d H:i:s';
-	/**
-	 *
-	 * @see https://stackoverflow.com/a/5367048/1469799
-	 * @param string $mysqldate
-	 * @return  string;
-	 */
-	public static function normalizeMysql(string $mysqldate):string
-	{
-		$phpdate = strtotime( $mysqldate );
-		return date( self::FORMAT, $phpdate );
-	}
+    const FORMAT = 'Y-m-d H:i:s';
+    /**
+     *
+     * @see    https://stackoverflow.com/a/5367048/1469799
+     * @param  string $mysqldate
+     * @return string;
+     */
+    public static function normalizeMysql(string $mysqldate):string
+    {
+        $phpdate = strtotime($mysqldate);
+        return date(self::FORMAT, $phpdate);
+    }
 
-	/**
-	 * @param string $mysqldate
-	 *
-	 * @return \DateTimeInterface
-	 * @throws \Exception
-	 */
-	public static function dateTimeFromMysql(string $mysqldate) : \DateTimeInterface
-	{
-		return new \DateTimeImmutable(self::normalizeMysql($mysqldate));
-	}
+    /**
+     * @param string $mysqldate
+     *
+     * @return \DateTimeInterface
+     * @throws \Exception
+     */
+    public static function dateTimeFromMysql(string $mysqldate) : \DateTimeInterface
+    {
+        return new \DateTimeImmutable(self::normalizeMysql($mysqldate));
+    }
 }
